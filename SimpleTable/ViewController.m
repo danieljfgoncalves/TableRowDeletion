ff8405fc34216da634c001c928adf4b29e609d9f
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-04-15 14:53:48 -0400

    first commit

---
+++ SimpleTable/ViewController.m
@@ -0,0 +1,48 @@
+//
+//  ViewController.m
+//  SimpleTable
+//
+//  Created by Simon Ng on 23/10/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+{
+    NSArray *recipes;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Initialize table data
+    recipes = @[@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    cell.imageView.image = [UIImage imageNamed:@"creme_brelee"];
+    
+    return cell;
+}
+
+@end
